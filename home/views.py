3976167b19d6bcc03be6d1d0e93d07042154d32a
rafaLeiteee <87039630+rafaLeiteee@users.noreply.github.com>
2023-01-04 12:42:18 -0300

    Add files via upload

--- home/views.py
+++ home/views.py
@@ -6,4 +6,62 @@ def index(request):
     return render(request, 'home/questionary.html')
 
 def xr_app(request):
-    return render(request, 'home/touch_gestures_page.html')
+    context = {}
+    aux = request.POST.get('name', None)
+    context['name'] = aux
+
+    aux = request.POST.get('p1', None)
+    context['p1'] = aux
+
+    aux = request.POST.get('p2', None)
+    context['p2'] = aux
+
+    aux = request.POST.get('p3', None)
+    context['p3'] = aux
+
+    aux = request.POST.get('p4', None)
+    context['p4'] = aux
+
+    aux = request.POST.get('p5', None)
+    context['p5'] = aux
+
+    aux = request.POST.get('p6', None)
+    context['p6'] = aux
+
+    aux = request.POST.get('p7', None)
+    context['p7'] = aux
+
+    aux = request.POST.get('p8', None)
+    context['p8'] = aux
+
+    aux = request.POST.get('p9', None)
+    context['p9'] = aux
+
+    aux = request.POST.get('p10', None)
+    context['p10'] = aux
+
+    aux = request.POST.get('p11', None)
+    context['p11'] = aux
+
+    aux = request.POST.get('p12', None)
+    context['p12'] = aux
+
+    aux = request.POST.get('p13', None)
+    context['p13'] = aux
+
+    aux = request.POST.get('p14', None)
+    context['p14'] = aux
+
+    aux = request.POST.get('p15', None)
+    context['p15'] = aux
+
+    aux = request.POST.get('p16', None)
+    context['p16'] = aux
+
+    aux = request.POST.get('p17', None)
+    context['p17'] = aux
+
+    aux = request.POST.get('p18', None)
+    context['p18'] = aux
+
+    return render(request, 'home/touch_gestures_page.html', context)
--- readme.md
+++ readme.md
@@ -1,12 +1,6 @@
 ### VR/AR Project
-In order to run this project, download it and with python installed and follow the steps:
-1. Create your venv with `python -m venv .venv`
-2. Activate it 
-    - (Windows Users): `.venv/Scripts/activate`
-    - (Unix Users): `.venv/bin/activate`
-3. Install all dependencies with `pip install -r requirements.txt`
-4. Run the local server `python manage.py runserver`
-5. You can see your application at localhost or commonly at http://127.0.0.1:8000/home/
+In order to run this project, download it and with python installed,
+run python `python -m http.server` inside this folder.
 - If you have an iPhone you may need to install this XRViewer
 - For Android users most browsers work
 
